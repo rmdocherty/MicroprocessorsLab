1815952ecb6b41ebc3d93dd50dc512e41906d3b8
rmdocherty <rmdocherty@users.noreply.github.com>
2021-02-18 15:33:37 +0000

    Moved 0xFF to W register instead of 0x63

--- main.s
+++ main.s
@@ -8,7 +8,7 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0
+	movlw 	0x0 
 	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
 loop:
@@ -16,7 +16,7 @@ loop:
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+	movlw   0xFF         ; move FF to working register
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
