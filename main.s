f5577e5e80be0411abb89415c73460d3fb3b6a85
rmdocherty <rmdocherty@users.noreply.github.com>
2021-02-19 11:01:15 +0000

    Added cascading delay routine - a second loop counter that decrements a number stored in FR 0x30 and calls the delay routine each time.

--- main.s
+++ main.s
@@ -10,6 +10,8 @@ main:
 start:
 	movlw   0xFF                ; Move 0xFF to W
 	movwf   0x20, A             ; Store 0xFF in FR 0x20
+	movwf   0xA, A
+	movwf   0x30, A             ; Store 0xA in FR 0x30 for second loop condition
 	movlw 	0x0 
 	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
@@ -17,10 +19,15 @@ delay:
         decfsz  0x20, A             ; Decrement literal at 0x20 until 0
 	bra     delay               ; Loop if not 0
 	return
+cascade:                            ; Cascading delay routine 
+	call    delay               ; Delay using delay subroutine
+        decfsz  0x30, A             ; Loop if not 0
+	bra     cascade             ; Repeat
+	return
 loop:
 	movff 	0x06, PORTC
 	incf 	0x06, W, A
-	call delay                  ; Call delay subroutine
+	call    cascade             ; Call cascade subroutine
 test:
 	movwf	0x06, A	            ; Test for end of loop condition
 	movlw   0xFF                ; move FF to working register
