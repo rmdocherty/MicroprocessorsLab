5597a4a5dd0fd0a804d732c91e1ee9ab01d094e0
rmdocherty <rmdocherty@users.noreply.github.com>
2021-02-20 18:39:25 +0000

    loop to show which port outputs are which

--- main.s
+++ main.s
@@ -10,5 +10,13 @@ main:
 start:
 	movlw 	0x0
 	movwf	TRISD, A	    ; Port D all outputs
-	movff   0x0F, PORTD
+	movlw   0xFF                ; Move FF to W
+	movwf   0x20, A             ; Move FF to 0x20 - # of loops
+	bra     test                ; Jump to test
+setlights:
+        movff   0x20, PORTD         ; Move value of 0x20 to PORTD
+	decf    0x20                ; Decrement the value in 0x20
+test:
+	cpfsgt  0x20, A             ; Check if 0 and if so skip 
+	bra     setlights
 	end	main
