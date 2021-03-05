70d94fca627c099b4e4f1fc55ed1424f62847f39
rmdocherty <rmdocherty@users.noreply.github.com>
2021-03-05 11:49:34 +0000

    Rewrote from ground up, now works
    First fixed the get col method, my compare statement was wrong way round so it always skipped to LHS. Now can write to both sides. Deleted extraneous code in GLCD_read. Fixed the mod and div operations. Added a mod_to_pattern which takes a modulus value and converts that to a binary number with the kth bit on. (needed because bsf doesn't work with a file register). Adjusted the clear screen method to work properly.

--- main.s
+++ main.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
 
-extrn	GLCD_Setup, GLCD_Draw
+extrn	GLCD_Setup, GLCD_Draw, GLCD_Write, GLCD_Test
     
 psect	udata_acs
 delay_count:	ds 1
@@ -13,6 +13,7 @@ rst: 	org 0x0
 
 main:
 	call	GLCD_Setup
+	;call	GLCD_Test
 	call	GLCD_Draw
 	end	rst
 
