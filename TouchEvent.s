a4b3ade1edb4187fb0d5df3debdabdfe19aaea2a
Vinstagramx <44545069+Vinstagramx@users.noreply.github.com>
2021-03-13 13:23:18 +0000

    Inversion fixed, and also offset in ADC readings accounted for (300 on x, 500 on y)
    Still has some scattered pixels, but core LCD touchscreen is working!

--- TouchEvent.s
+++ TouchEvent.s
@@ -14,18 +14,23 @@ x_voltage_h:	ds 1	; Cannot operate on SFR ADRES, need to move two 8-bit numbers 
 x_voltage_l:	ds 1	; Move two numbers for each coordinate before doing integer division
 y_voltage_h:	ds 1
 y_voltage_l:	ds 1
+inv_const:	ds 1	; Inversion constant - 64 for y
     
     
 psect	glcd_code,class=CODE
     
 ; Subtraction - high 8-bit number incremented by 1, in order to have decfsz skip once lower 8-bit reaches 0
 ; i.e. (high) 0001 (low) 0003 would cause a skip (and coord save) with next subtract, but would now be (high) 0002 (low) 0003
+; Need to subtract by an offset of 300 = 1 00101100 (x) and 500 = 1 11110100 (y)
+; +1 -1 on high 8-bit number = no incf needed 
 move_adres_X:		; Should be called before Convert_X
 	movff   ADRESL, x_voltage_l	; Moving ADRES value to x_voltage (low,high)
 	movff   ADRESH, x_voltage_h
 	movlw	0x00
 	movwf	x_counter, A		; Reset x-counter to zero
-	incf	x_voltage_h, A		; Increment high voltage by 1
+;	incf	x_voltage_h, A		; Increment high voltage by 1
+	movlw	00101100B
+	subwf	x_voltage_l, F, A
 	return
 		
 Convert_X:
@@ -35,15 +40,19 @@ Convert_X:
         btfss   STATUS, 0		; Check if carry bit (bit 0 of STATUS) is 1, if not, skip next
         decfsz  x_voltage_h, F, A	; If carry = 1, decrement high 8-bit
 	bra	Convert_X		; Loop again
-	movf	x_counter, W		; Move counter value to W
+	movf	x_counter, W		; Move counter value to W if high 8-bit is zero
 	return
 
 move_adres_Y:		; Should be called before Convert_Y
 	movff   ADRESL, y_voltage_l	; Moving ADRES value to y_voltage (low,high)
 	movff   ADRESH, y_voltage_h
+	movlw	0x40			; Moving 64 to inversion constant (for subtraction later)
+	movwf	inv_const, A
 	movlw	0x00
 	movwf	y_counter, A		; Reset y-counter to zero
-	incf	y_voltage_h, A		; Increment high voltage by 1
+;	incf	y_voltage_h, A		; Increment high voltage by 1
+	movlw	11110100B
+	subwf	y_voltage_l, F, A
 	return
 		
 Convert_Y:
@@ -53,6 +62,7 @@ Convert_Y:
         btfss   STATUS, 0		; Check if carry bit (bit 0 of STATUS) is 1, if not, skip next
         decfsz  y_voltage_h, F, A	; If carry = 1, decrement high 8-bit
 	bra	Convert_Y		; Loop again
-	movf	y_counter, W		; Move counter value to W
+	movf	y_counter, W		; Move counter value to W if high 8-bit is 0
+	subwf	inv_const, W		; Subtract counter from 64 and place back in W
 	return
 	
